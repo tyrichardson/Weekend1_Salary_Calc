49453e54477b9d602974e1b478e37288895c9925
Ty Richadson <tyLrichardson63@gmail.com>
2018-03-09 16:45:24 -0600

    initial commit of UI

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log('ty js');
+
+
+
+
+
+
+
+
+
+//
+// let redClicks = 0;
+//
+// $(document).ready(readyNow);
+//
+// function readyNow() {
+//   console.log('in readyNow function');
+//   buttonClick();
+// }
+//
+// function buttonClick(){
+//   $('#redButton').on('click', redButtonClicked);
+//  }
+//
+// function redButtonClicked() {
+//   console.log('I clicked redButton');
+//   let addRedBox = ('<div class="redBox"></div>');
+//   $('.boxes').append(addRedBox);
+//   redClicks += 1;
+//   redCounter();
+// }
+//
+// function redCounter(){
+//   console.log(redClicks);
+//   $('#redCounter').empty();
+//   $('#redCounter').append('Red count: ' + redClicks);
+// }
