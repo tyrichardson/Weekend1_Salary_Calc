5830c6aca142702663487eb2f2c2307cddcc2351
Ty Richadson <tyLrichardson63@gmail.com>
2018-03-09 20:35:24 -0600

    first draft of js script

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
 console.log('ty js');
 
+//declare global variables
+let totalSalaries = 0;
+let employeeArray = [];
 
+//define a new class
+class Employee {
+  constructor(firstNameIn, lastNameIn, idIn, titleIn, annualSalaryIn){
+    this.firstName = firstNameIn;
+    this.lastName = lastNameIn;
+    this.id = idIn;
+    this.title = titleIn;
+    this.annualSalary = annualSalaryIn;
+  }//end of constructor
+}//end of class Employee
 
+//wait, then confirm the DOM loaded
 $(document).ready(readyNow);
 
+//kickoff the application
 function readyNow() {
-console.log('in readyNow function');
-//buttonClick();
+  console.log('entered readyNow function');
+  buttonClick();
+}
+
+//define the listerner
+function buttonClick(){
+  console.log('entered buttonClick function');
+  $('#submitButton').on('click', submitButtonClicked);
+ }
+
+//define the actions triggered by the event
+function submitButtonClicked() {
+  console.log('I clicked submitButton');
+  //access the inputted employee data
+  let firstName = $('#firstName').val();
+  let lastName = $('#lastName').val();
+  let id = $('#id').val();
+  let title = $('#title').val();
+  let annualSalary = $('#annualSalary').val();
+  console.log('values from user: ',firstName, lastName, id, title, annualSalary);
+  //instantiate a new instance of the Employee class with inputted employee data
+  addEmployee = new Employee(firstName, lastName, id, title, annualSalary);
+  console.log('New employee object from class: ', addEmployee);
+  //populate table on the DOM with inputted values
+  $('.tdFirstName').text(firstName);
+  $('.tdLastName').text(lastName);
+  $('.tdID').text(id);
+  $('.tdTitle').text(title);
+  $('.tdAnnualSalary').text(annualSalary);
+  //populate the employeeArray with the inputted values
+  employeeArray.push(addEmployee);
+  console.log('The update to employeeArray: ', employeeArray);
+  
 
 }
 
-
-
-
-
-//
-// let redClicks = 0;
-//
-//
-//
-//
-//
-// function buttonClick(){
-//   $('#redButton').on('click', redButtonClicked);
-//  }
-//
-// function redButtonClicked() {
-//   console.log('I clicked redButton');
-//   let addRedBox = ('<div class="redBox"></div>');
-//   $('.boxes').append(addRedBox);
-//   redClicks += 1;
-//   redCounter();
-// }
 //
 // function redCounter(){
 //   console.log(redClicks);
